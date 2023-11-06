dc586256c38e34ed57cab4ea5db0fe7c47b11bf4
Santy1899 <santhoshmathavan.k@propvr.ai>
2023-11-06 15:42:12 +0530

    Fix: Folder Redifned

--- index.js
+++ index.js
@@ -14,105 +14,129 @@ app.use(bodyParser.json({
     limit: '50mb', extended: true
 }));
 app.use(bodyParser.urlencoded({
-limit: '50mb',
-parameterLimit: 100000,
-extended: true 
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true
 }));
 
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }))
 
 app.use((req, res, next) => {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-next();
-}) 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
 
 const port = 5001;
 
 
-app.post("/download",async(req,res)=>{
+app.post("/download", async (req, res) => {
     const body = req.body
     console.log(body)
     try {
         const response = await axios({
-        method: 'get',
-        url: body.url,
-        responseType: 'stream',
-        headers: { 
-            'Content-Type': 'application/json', 
-            'Authorization': 'Bearer '+CESIUM_API_KEY
-        },
+            method: 'get',
+            url: body.url,
+            responseType: 'stream',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + CESIUM_API_KEY
+            },
         });
 
-        
+
         const writer = fs.createWriteStream(body.destinationPath);
 
         response.data.pipe(writer);
 
         return new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-        }).then(()=>{
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        }).then(() => {
             // Unzip the downloaded file
             fs.createReadStream(body.destinationPath)
-            .pipe(unzipper.Extract({ path: body.extractPath })) // Specify the extraction path
-            .on('finish', () => {
-            console.log('File Unzipped Successfully');
-            fs.unlinkSync(body.destinationPath)
-            console.log("Zip File deleted Successfully");
-            res.send({status:1, message:"File Downloaded and Unzipped Successfully!!!!"})
+                .pipe(unzipper.Extract({ path: body.extractPath })) // Specify the extraction path
+                .on('finish', () => {
+                    console.log('File Unzipped Successfully');
+                    fs.unlinkSync(body.destinationPath)
+                    console.log("Zip File deleted Successfully");
+                    res.send({ status: 1, message: "File Downloaded and Unzipped Successfully!!!!" })
 
-            // Respond or perform further actions here
-        });
+                    // Respond or perform further actions here
+                });
         })
-        
+
 
     } catch (error) {
         console.error('File download failed:', error);
-        res.send({status:0, message:"File Download Error!!!!!"})
+        res.send({ status: 0, message: "File Download Error!!!!!" })
     }
 })
 
-app.post("/thumbnailDownload", async(req,res)=>{
+app.post("/thumbnailDownload", async (req, res) => {
     // Assuming you have received the response in a variable called 'response'
     const bodyData = req.body
-    const fileData  = bodyData.data
+    const fileData = bodyData.data
     const fileName = req.body.fileName
+    const imageType = req.body.type;
     const randomId = "test123"
     // Convert the ArrayBuffer to a Buffer
     const buffer = Buffer.from(fileData);
 
     // Define the path and filename for the saved file
-    const localDirPath = '../../Asset-Server/dist/public/assets/VRTour/Images_Testing/'+randomId;
-    //const localFilePath = path.join(__dirname, 'downloads/', fileName);
-	//console.log(localFilePath)
-    // Create the directory if it doesn't exist
-   fs.mkdir(localDirPath,{recursive:true},(error)=>{
-    if(error){
-    console.log("directory error", error)
+    const localDirPath = ''
+    if(imageType=="ProjectThumbs"){
+        console.log("ProjectThumbs");
+        let subDirPath = imageType;
+        localDirPath = '../../Asset-Server/dist/public/assets/'+ subDirPath;
     }
     else{
-    console.log("New Directory")
-    const localFilePath = '../../Asset-Server/dist/public/assets/VRTour/Images_Testing/'+randomId+'/'+fileName;
-    // Save the Buffer as a file
-    fs.writeFile(localFilePath, buffer, (err) => {
-	console.log("sdfsdfsdfsdfsdf")        
-if (err) {
-        console.error('Error saving the file:', err);
-        //   res.status(500).send('Error saving the file');
-        } else {
-console.log("else")
-        const thumbnailurl = 'https://iron-storageserver.propvr.in/public/assets/VRTour/Images_Testing/'+randomId+'/'+fileName
-        res.send({status:1, thumbnailUrl:thumbnailurl});
-        }
-    });
+        console.log("VRTour");
+        let subDirPath = imageType +'/Images_Testing/'+ randomId;
+        localDirPath = '../../Asset-Server/dist/public/assets/'+ subDirPath;
     }
-})
+    // const localDirPath = '../../Asset-Server/dist/public/assets/VRTour/Images_Testing/' + randomId;
+    //const localFilePath = path.join(__dirname, 'downloads/', fileName);
+    //console.log(localFilePath)
+    // Create the directory if it doesn't exist
+    fs.mkdir(localDirPath, { recursive: true }, (error) => {
+        if (error) {
+            console.log("directory error", error)
+        }
+        else {
+            console.log("New Directory")
+            const localFilePath=''
+            const subPath=''
+            if(imageType=="ProjectThumbs"){
+                console.log("ProjectThumbs");
+                subPath = imageType +'/' + fileName
+                localFilePath = '../../Asset-Server/dist/public/assets/'+ subPath;
+            }
+            else{
+                console.log("VRTour");
+                subPath = imageType +'/Images_Testing/'+ randomId + '/' + fileName
+                localFilePath = '../../Asset-Server/dist/public/assets/'+ subPath;
+            }
+            // Save the Buffer as a file
+            fs.writeFile(localFilePath, buffer, (err) => {
+                console.log("sdfsdfsdfsdfsdf")
+                if (err) {
+                    console.error('Error saving the file:', err);
+                    //   res.status(500).send('Error saving the file');
+                } else {
+                    console.log("else")
+                    const thumbnailurl = 'https://iron-storageserver.propvr.in/public/assets/'+subPath
+                    console.log(thumbnailurl)
+                    res.send({ status: 1, thumbnailUrl: thumbnailurl });
+                }
+            });
+        }
+    })
 })
 
-app.get("/",(req,res)=>{
-    res.send({status:1})
+app.get("/", (req, res) => {
+    res.send({ status: 1 })
 })
 
 //listen server
